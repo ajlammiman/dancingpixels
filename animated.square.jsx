9c339e4434163c8a76c4781272a9a90720d8cb57
alammiman <alammiman@Companynet.org>
2020-01-10 22:32:08 +0000

    _

---
+++ animated.square.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Square = styled.div`
+   background-color:#2bb8e4;
+   width:20px;
+   height:20px;
+`
+
+
+export const AnimatedSquare = () => {
+    return <Square />
+}
